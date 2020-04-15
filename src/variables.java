9d8ce3ea9dc42fa3da96d2ce2d6937f73d1cc866
aminahaid <aminahaid@gmail.com>
2020-04-15 16:44:31 -0500

    xvariables commit

---
+++ src/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args)
+    {    int testScoreInitialized = 50;
+    double priceTag = 24.99;
+    String userName = "Bobby jonhson";
+    System.out.println("Test Score Initialized: "+testScoreInitialized +"\n"+"Price Tag: " + priceTag+"\n"+"User Name: "+userName);
+
+
+
+
+
+
+    }
+}
